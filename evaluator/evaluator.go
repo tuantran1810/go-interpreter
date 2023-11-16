71e2794f13bc630e11d88b0c0d2b389568dc8e20
tuantran <tuantran@chotot.vn>
2023-11-16 22:11:12 +0700

    update new code

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,14 +5,30 @@ import (
 	"github.com/tuantran1810/go-interpreter/object"
 )
 
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
+	// fmt.Printf("------> %+v --- node.type = %v\n", node, reflect.TypeOf(node))
 	switch node := node.(type) {
 	case *ast.Program:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		if node.Value {
+			return TRUE
+		} else {
+			return FALSE
+		}
 	}
 	return nil
 }
@@ -23,4 +39,37 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	obj := right.(*object.Integer)
+	obj.Value = -obj.Value
+	return obj
 }
--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/tuantran1810/go-interpreter/evaluator"
 	"github.com/tuantran1810/go-interpreter/lexer"
 	"github.com/tuantran1810/go-interpreter/parser"
 )
@@ -30,8 +31,9 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
+		obj := evaluator.Eval(program)
 
-		io.WriteString(out, program.String())
+		io.WriteString(out, obj.Inspect())
 		io.WriteString(out, "\n")
 	}
 }
