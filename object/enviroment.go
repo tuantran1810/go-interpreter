59e68419e1d20514885ac7cd6f5b20a5984cd00a
tuantran <tuantran@chotot.vn>
2023-11-28 00:29:45 +0700

    update new code

--- object/enviroment.go
+++ object/enviroment.go
@@ -2,6 +2,7 @@ package object
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func NewEnvironment() *Environment {
@@ -9,8 +10,16 @@ func NewEnvironment() *Environment {
 	return &Environment{store: s}
 }
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s, outer: outer}
+}
+
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
